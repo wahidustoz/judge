278d6f8d4edace3792b6c4b540d20402d5920c53
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-09-15 06:50:40 +0900

    Added one more test

--- tests/Ilmhub.Judge.Client.Tests.Integration/PingTest.cs
+++ tests/Ilmhub.Judge.Client.Tests.Integration/PingTest.cs
@@ -10,30 +10,11 @@ public class PingTest
     private readonly ServiceProvider provider;
 
     public PingTest()
-    {
-        var configuration = ConfigurationMockFactory.Create(new Dictionary<string, string>
-        {
-            { "JudgeServer:BaseUrl", "http://localhost:12358" },
-            { "JudgeServer:Token", "dccf62d6-3628-4ad0-a30e-49d159e54136" }
-        });
-        var services = new ServiceCollection();
-        services.AddSingleton(configuration);
-        services.AddJudgeServerClient(configuration);
-        provider = services.BuildServiceProvider();
-    }
+        => provider = SetupServiceProvider("http://localhost:12358", "dccf62d6-3628-4ad0-a30e-49d159e54136");
 
     [Fact]
     public async Task ValidPingSucceedsAsync()
     {
-        var configuration = ConfigurationMockFactory.Create(new Dictionary<string, string>
-        {
-            { "JudgeServer:BaseUrl", "http://localhost:12358" },
-            { "JudgeServer:Token", "dccf62d6-3628-4ad0-a30e-49d159e54136" }
-        });
-        var services = new ServiceCollection();
-        services.AddSingleton(configuration);
-        services.AddJudgeServerClient(configuration);
-        var provider = services.BuildServiceProvider();
         var client = provider.GetRequiredService<IJudgeServerClient>();
 
         var response = await client.PingAsync();
@@ -42,21 +23,39 @@ public class PingTest
     }
 
     [Fact]
-    public async Task PingWithInvalidTokenFailsAsync()
+    public async Task PingWithInvalidTokenThrowsPingFailedExceptionAsync()
     {
-        var configuration = ConfigurationMockFactory.Create(new Dictionary<string, string>
-        {
-            { "JudgeServer:BaseUrl", "http://localhost:12358" },
-            { "JudgeServer:Token", "POTATO" }
-        });
-        var services = new ServiceCollection();
-        services.AddSingleton(configuration);
-        services.AddJudgeServerClient(configuration);
-        var provider = services.BuildServiceProvider();
+        var provider = SetupServiceProvider("http://localhost:12358", "POTATO");
         var client = provider.GetRequiredService<IJudgeServerClient>();
 
         var pingTask = () => client.PingAsync().AsTask();
-        
+
         await Assert.ThrowsAsync<PingFailedException>(pingTask);
+    }
+
+    [Fact]
+    public async Task PingWithInvalidUrlThrowsHttpRequestExceptionAsync()
+    {
+        var provider = SetupServiceProvider("http://localhost:123", "POTATO");
+        var client = provider.GetRequiredService<IJudgeServerClient>();
+
+        var pingTask = () => client.PingAsync().AsTask();
+
+        await Assert.ThrowsAsync<HttpRequestException>(pingTask);
+    }
+
+    private ServiceProvider SetupServiceProvider(string baseUrl, string token)
+    {
+        var configuration = ConfigurationMockFactory.Create(new Dictionary<string, string>
+            {
+                { "JudgeServer:BaseUrl", baseUrl },
+                { "JudgeServer:Token", token }
+            });
+
+        var services = new ServiceCollection();
+        services.AddSingleton(configuration);
+        services.AddJudgeServerClient(configuration);
+
+        return services.BuildServiceProvider();
     }
 }
