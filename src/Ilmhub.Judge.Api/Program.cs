0bfdbce27003ac6fe6606f5f210379586b4e8c24
Mehriddin <mr.mekhriddin@gmail.com>
2023-09-27 22:19:17 +0500

    Fluent validation for Judge endpoint

--- src/Ilmhub.Judge.Api/Program.cs
+++ src/Ilmhub.Judge.Api/Program.cs
@@ -1,9 +1,13 @@
 using Ilmhub.Judge.Sdk;
 using Ilmhub.Judge.Sdk.Options;
 using Ilmhub.Judge.Api;
+using FluentValidation;
+using Ilmhub.Judge.Api.Dtos;
+using static Ilmhub.Judge.Api.Dtos.JudgeRequestDto;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddTransient<IValidator<JudgeRequestDto>, JudgeRequestValidator>();
 builder.Services.AddIlmhubJudge(builder.Configuration.GetSection($"{IlmhubJudgeOptions.Name}"));
 builder.Services.SetupOpenTelemetry(builder.Configuration);
 builder.Services.AddLogging(logBuilder => logBuilder.ConfigureOpenTelemetryLogging(builder.Configuration));
