ab315fe2909e7e60542ae62d37f401ee5ed1ff34
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-09-29 04:22:19 +0900

    fixed validatin, ratelimting and renamed stuff

--- src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
+++ src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
@@ -3,32 +3,20 @@ using FluentValidation;
 
 namespace Ilmhub.Judge.Api.Filters;
 
-public static class ValidationFilterRouteHandlerBuilderExtensions
-{
-    public static IEndpointConventionBuilder WithAsyncValidation<T>(this RouteHandlerBuilder builder) where T : class
-    {
-        builder.AddEndpointFilter<FluentAsynValidationFilter<T>>();
-        return builder;
-    }
-}
-
 public class FluentAsynValidationFilter<T> : IEndpointFilter where T : class
 {
     public async ValueTask<object> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
-        T argToValidate = context.GetArgument<T>(0);
+        var argumentOrNull = context.Arguments.FirstOrDefault(a => a is T);
         IValidator<T> validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
 
-        if (validator is not null)
+        if(validator is not null && argumentOrNull is T argumentToValidate)
         {
-            var validationResult = await validator.ValidateAsync(argToValidate!);
+            var validationResult = await validator.ValidateAsync(argumentToValidate);
             if (validationResult.IsValid is false)
-                return Results.ValidationProblem(
-                    errors: validationResult.ToDictionary(),
-                    statusCode: (int)HttpStatusCode.UnprocessableEntity);
+                return Results.ValidationProblem(validationResult.ToDictionary(), statusCode: (int)HttpStatusCode.UnprocessableEntity);
         }
-
-        // Otherwise invoke the next filter in the pipeline
+        
         return await next.Invoke(context);
     }
 }
