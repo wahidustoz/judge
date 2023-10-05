420c874239fcd22c0504e4f7fe3b6ab23451eb1d
sarvar-swe <s.azodov.software.engineer@gmail.com>
2023-10-06 00:06:00 +0500

    Logging: LogInformation and LogError added to FluentAsyncValidationFilter

--- src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
+++ src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
@@ -5,20 +5,37 @@ namespace Ilmhub.Judge.Api.Filters;
 
 public class FluentAsynValidationFilter<T> : IEndpointFilter where T : class
 {
+    private readonly ILogger<FluentAsynValidationFilter<T>> logger;
+
+    public FluentAsynValidationFilter(ILogger<FluentAsynValidationFilter<T>> logger)
+    {
+        this.logger = logger;
+    }
+
     public async ValueTask<object> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
+        logger.LogInformation("Executing FluentAsynValidationFilter...");
         var argumentOrNull = context.Arguments.FirstOrDefault(a => a is T);
         IValidator<T> validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
 
-        if(argumentOrNull is T argumentToValidate is false)      
+        if(argumentOrNull is T argumentToValidate is false)
+        {
+            logger.LogError("Request is missing required object.");
             return Results.BadRequest("Request is missing required object.");
+        }      
 
-        if (validator is not null)
+        if(validator is not null)
         {
             var validationResult = await validator.ValidateAsync(argumentToValidate);
-            if (validationResult.IsValid is false)
+            if(validationResult.IsValid is false)
+            {
+                logger.LogError("Validation failed: {Errors}", validationResult.Errors);
                 return Results.ValidationProblem(validationResult.ToDictionary(), statusCode: (int)HttpStatusCode.UnprocessableEntity);
+            }
         }
-        return await next.Invoke(context);
+
+        var result = await next.Invoke(context);
+        logger.LogInformation("FluentAsynValidationFilter executed successfully.");
+        return result;
     }
 }
