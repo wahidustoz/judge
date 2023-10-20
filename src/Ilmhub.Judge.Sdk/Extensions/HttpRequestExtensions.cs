2166e2747d6cb2adc8c79147ee213be46bb6f535
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-10-20 18:35:23 +0900

    Added missing files to commit

--- src/Ilmhub.Judge.Sdk/Extensions/HttpRequestExtensions.cs
+++ src/Ilmhub.Judge.Sdk/Extensions/HttpRequestExtensions.cs
@@ -4,5 +4,5 @@ public static class HttpRequestExtensions
 {
     public static bool IsClientError(this HttpRequestException ex)
         => ex.StatusCode.HasValue
-        && (int)ex.StatusCode is (>= 400 and < 500);
+        && (int)ex.StatusCode is >= 400 and < 500;
 }
