f9b5119c4b8f284a0bcbbbdef45c9c685d5565db
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-10-20 18:42:47 +0900

    Fixed SDK project formatting (#45)

--- src/Ilmhub.Judge.Sdk/Extensions/HttpRequestExtensions.cs
+++ src/Ilmhub.Judge.Sdk/Extensions/HttpRequestExtensions.cs
@@ -3,6 +3,6 @@ namespace Ilmhub.Judge.Sdk.Extensions;
 public static class HttpRequestExtensions
 {
     public static bool IsClientError(this HttpRequestException ex)
-        => ex.StatusCode.HasValue 
-        && (int)ex.StatusCode is (>= 400 and < 500);
+        => ex.StatusCode.HasValue
+        && (int)ex.StatusCode is >= 400 and < 500;
 }
--- src/Ilmhub.Judge.Sdk/Extensions/LoggingExtensions.cs
+++ src/Ilmhub.Judge.Sdk/Extensions/LoggingExtensions.cs
@@ -6,8 +6,8 @@ namespace Ilmhub.Judge.Sdk;
 public static partial class LoggingExtensions
 {
     [LoggerMessage(
-        EventId = 0, 
-        Level = LogLevel.Warning, 
+        EventId = 0,
+        Level = LogLevel.Warning,
         Message = "JUDGE CLIENT: failed to execute operation {operationName}")]
     public static partial void LogException(this ILogger logger, Exception ex, [CallerMemberName] string operationName = null);
 }
