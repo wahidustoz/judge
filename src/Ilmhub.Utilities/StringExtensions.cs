7686b55e3554e11cf7dca45eae57ebfcdf893d04
Wahid Abduhakimov <wakhid2802@gmail.com>
2023-10-21 19:26:59 +0900

    Fix formatting for Utilities project. (#51)

--- src/Ilmhub.Utilities/StringExtensions.cs
+++ src/Ilmhub.Utilities/StringExtensions.cs
@@ -7,7 +7,7 @@ public static class StringExtensions
 {
     public static string Md5(this string original)
     {
-        if(string.IsNullOrWhiteSpace(original))
+        if (string.IsNullOrWhiteSpace(original))
             return original;
 
         using MD5 hasher = MD5.Create();
