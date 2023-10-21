2d8c424bc27b5425368370389e9495fb5b0a67bb
Mehriddin <71074521+mekhriddinbozorov@users.noreply.github.com>
2023-10-21 14:18:19 +0500

    Implement FluentValidationFilterLoggings (#46)
    * Implement FluentValidationFilterLoggings
    
    * fix
    
    * fix:filename
    
    * fix:FilterFailed
    
    * Update src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
    
    * Update src/Ilmhub.Judge.Api/Logging/ValidationFilterLoggings.cs
    
    ---------
    
    Co-authored-by: Wahid Abduhakimov <wakhid2802@gmail.com>

--- src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
+++ src/Ilmhub.Judge.Api/Filters/FluentAsynValidationFilter.cs
@@ -1,24 +1,41 @@
 using System.Net;
 using FluentValidation;
+using Ilmhub.Judge.Api.Logging;
 
 namespace Ilmhub.Judge.Api.Filters;
 
-public class FluentAsynValidationFilter<T> : IEndpointFilter where T : class
+public class AsyncFluentValidationFilter<T> : IEndpointFilter where T : class
 {
+    private readonly ILogger<AsyncFluentValidationFilter<T>> logger;
     public async ValueTask<object> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
-        var argumentOrNull = context.Arguments.FirstOrDefault(a => a is T);
-        IValidator<T> validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
-
-        if(argumentOrNull is T argumentToValidate is false)      
-            return Results.BadRequest("Request is missing required object.");
-
-        if (validator is not null)
+        try
         {
-            var validationResult = await validator.ValidateAsync(argumentToValidate);
-            if (validationResult.IsValid is false)
-                return Results.ValidationProblem(validationResult.ToDictionary(), statusCode: (int)HttpStatusCode.UnprocessableEntity);
+            var argumentOrNull = context.Arguments.FirstOrDefault(a => a is T);
+            IValidator<T> validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
+
+            if(argumentOrNull is T argumentToValidate is false)
+                return Results.BadRequest("Request is missing required object.");
+            
+            logger.LogValidationStarted(nameof(T));
+            if(validator is not null)
+            {
+                var validationResult = await validator.ValidateAsync(argumentToValidate);
+                if(validationResult.IsValid is false)
+                {
+                    logger.LogValidationFailed(
+                        targetType: nameof(T), 
+                        errors: validationResult.Errors.ToDictionary(x => x.PropertyName, x => x.ErrorMessage));
+                    return Results.ValidationProblem(validationResult.ToDictionary(), statusCode: (int)HttpStatusCode.UnprocessableEntity);
+                }
+            }
+            logger.LogValidationCompleted(nameof(T));
+            return await next.Invoke(context);
         }
-        return await next.Invoke(context);
+        catch (Exception ex)
+        {
+            logger.LogValidationException(nameof(T), ex);
+            return Results.Problem(ex.Message, statusCode: (int)HttpStatusCode.InternalServerError);
+        }
     }
 }
---
+++ src/Ilmhub.Judge.Api/Logging/ValidationFilterLoggings.cs
@@ -0,0 +1,37 @@
+namespace Ilmhub.Judge.Api.Logging;
+public static partial class ValidationFilterLoggings
+{
+    [LoggerMessage(
+        EventId = 0,
+        Level = LogLevel.Trace,
+        Message = "Fluent validation started for type {targetType}.")]
+    public static partial void LogValidationStarted(
+        this ILogger logger,
+        string targetType);
+
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Trace,
+        Message = "Fluent validation for {targetType} is successful.")]
+    public static partial void LogValidationCompleted(
+        this ILogger logger,
+        string targetType);
+
+    [LoggerMessage(
+        EventId = 4,
+        Level = LogLevel.Trace,
+        Message = "Fluent validation failed for {targetType} with error: {errors}.")]
+    public static partial void LogValidationFailed(
+        this ILogger logger,
+        string targetType,
+        Dictionary<string, string> errors);
+
+    [LoggerMessage(
+        EventId = 5,
+        Level = LogLevel.Warning,
+        Message = "Error occured during fluent validation for {targetType}.")]
+    public static partial void LogValidationException(
+        this ILogger logger,
+        string targetType,
+        Exception exception);
+}
